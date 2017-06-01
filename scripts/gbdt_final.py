1fce16f8bd4a64627c0fa60f19e4d008441d64d1
YuTing <spylyt@qq.com>
2017-06-01 08:45:05 +0800

    Update gbdt_final.py
    修改到可以运行

--- scripts/gbdt_final.py
+++ scripts/gbdt_final.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Thu Jun  1 08:24:34 2017
+
+@author: Administrator
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sat May 27 23:10:03 2017
 
 @author: L
@@ -28,135 +35,126 @@ GLOBAL = {}
 train_f = r'../train/'
 arima_path = r'../arima/'
 this = 'gbdt'
+
+def gg(start='10/18/2016 06:00',test_size=12,freq='20Min',periods=12,
+          days=7,predict=False,test=False,train=False,this=''):
+    
+    outputlist = []
+    
+    df,train_seq = load(start=start,freq=freq,normalize = False,periods=periods,days=days,pat=True)
+
+    df.loc[:,'time_window_s']=pd.to_datetime(df['time_window_s'],format=r'%Y/%m/%d %H:%M:%S')
+#    df.loc[:,'residual'] = np.log(df['travel_time'])-np.log(df['pred'])
+
+    hour = train_seq.hour[0]#param
+    if predict:
+        train_seq = train_seq + Hour(2)
+#    df.to_csv(r'bp.csv',index=False)
+
+    inter = df.groupby('intersection_id')
+    for t,tgroup in inter:
+        ds = tgroup.groupby('tollgate_id')
+        for d,dgroup in ds:
+            batch_x = dgroup.set_index('time_window_s').loc[train_seq,:]
+            print(batch_x['travel_time'])
+            batch_x['travel_time'] = modification(batch_x['travel_time'],method=3,show=False)
+            batch_x.loc[:,'residual'] = np.log(batch_x['travel_time'])
+            while np.any(pd.isnull(batch_x)):
+                print('nan found ',t,'',d)
+                print(batch_x.head())
+                batch_x = batch_x.bfill()
+                batch_x = batch_x.ffill()
+#                print(batch_x.head())
+#                sys.exit()
+            columns = ['Friday','Monday','Saturday','Sunday','Thursday','Tuesday','Wednesday','midnight','morning',\
+            'noon','afternoon','evening','w0','w20','w40','000','100','102','103','104','105','107','108','109','110',\
+            '111','112','115','116','119','120','122','123','out_link_cross_count','in_link_cross_conut','length',\
+            'link_count','1_length','2_length','3_length','4_length','pressure','sea_pressure','wind_direction',\
+            'wind_speed','temperature','rel_humidity','precipitation','pattern']
+#            print(batch_x.dtypes)
+#            print(batch_x[columns])
+            
+            x_data = bp_pca(np.array(batch_x[columns]))
+#    x_data = np.array(batch_x[['pressure','sea_pressure','wind_direction','wind_speed',
+#                                'temperature','rel_humidity','precipitation',
+#                                'dayofweek','hour','minute','direction','pattern']])
+                   
+            y_data = np.array(batch_x.loc[:,'residual'])#修改为残差
+            y_data = y_data.reshape((len(y_data),1))
+    
+            if train:
+                gbdt = kdd_gbdt()
+                gbdt.save_name = ''.join([this,str(t),str(d),str(0)])
+                gbdt.train_times = 1
+                
+                best_params = gbdt.choose(x_data,y_data)
+                print(best_params)
+                gbdt.params = best_params
+                gbdt.train(x_data,y_data)
+                gbdt.save_model()
+            else:
+                gbdt = kdd_gbdt()
+                gbdt.save_name = ''.join([this,str(t),str(d),str(hour)])
+                gbdt.load_model()
+                pred = gbdt.predict(x_data)
+                print('pred length',len(pred))
+                print('true length ',len(y_data))
+                pred = pd.Series(data=np.array(pred).flatten(),index = train_seq)
+                pred = modification(pred,method=3,show=True)
+                batch_x.loc[:,'bp'] = np.exp(pred)
+                outputlist.append(batch_x[['intersection_id','tollgate_id','travel_time','gbdt']]) 
+    if len(outputlist)>0:
+        print('ouputlist',len(outputlist))
+        df = pd.concat(outputlist)
+        df.index.name = 'time_window_s'
+        return df
+    
+    
 if __name__ == '__main__':
-    GLOBAL = cache(GLOBAL)
-    print(GLOBAL)
+#    GLOBAL = cache(GLOBAL)
+#    print(GLOBAL)
     traints = ['10/8/2016 06:00','10/8/2016 15:00']
     testts = ['10/25/2016 06:00','10/25/2016 15:00']
     trainlist = []
     testlist=[]
     bp_list = []
-####    #arima train and test
-    for i in range(len(traints)):
-        GLOBAL,traindf = train(GLOBAL,start=traints[i],freq='20Min',days=17,periods=12)
-        pred4hour,pred2hour = arima_pred(GLOBAL,start=testts[i],freq='20Min',periods=12,days=7)
-        trainlist.append(traindf)
-        testlist.append(pred2hour)
-        bp_list.append(pred4hour)
-    df = pd.concat(testlist)
-    df.index.name = 'time_window_s'
-    df.to_csv(r'time_final.csv')#预测集结果
-    df = pd.concat(bp_list)
-    df.to_csv(r'time_final_bp.csv')#预测集4小时结果
-    df = pd.concat(trainlist)
-    df.to_csv(r'time_total.csv')#训练集预测结果
-    GLOBAL = cache(GLOBAL)
-    final_agg()
     
-#    for i in range(len(traints)):
-#        bp_tt(start=traints[i],days=17,train=True)
-##        
-#    #GBDT训练
-#    for i in range(len(traints)):
-#        gbdt_tt(start=traints[i],days=17)
+    #GBDT训练
+    gg(start='9/19/2016 00:00',days=30,train=True,this = this,periods=72)
         
-    #####训练集BP修正
-#    dflist = []
-#    for i in range(len(traints)):
-#        df = bp_tt(start=traints[i],predict=False,days=17)
-#        dflist.append(df)
-#    df = pd.concat(dflist)
-#    df.to_csv(r'bp_result.csv')
-#    
-#    ###GBDT 测试
-##    dflist = []
-##    for i in range(len(traints)):
-##        df = bp_test(start=traints[i],predict=False,days=17)
-##        dflist.append(df)
-##    df = pd.concat(dflist)
-##    df.to_csv(r'bp_result.csv')
-#        
-#    ###使用model继续训练
-#    
-#    ###evalute
-#    df = load_volume(fdir=r'bp_result.csv')
-#    df = df[pd.notnull(df['bp'])]
-#    for i in range(len(traints)):
-#        evalute(df,start=traints[i],predict=False,days=17)
-#
+    ####训练集BP修正
+    
+    ##GBDT 测试
 ##############################################################
-#    '''
-#        在测试集上，使用BP预训练模型测试
-#        测试集BP修正
-#    '''
-    predict = True
+    predict = False
     test = True
     day = 7
-    reg = False
     if test:
         ll = testts
     else:
         ll = traints
     dflist = []
     for i in range(len(ll)):
-        df = bp_tt(start=ll[i],test=test,predict=predict,days=day)
+        df = gg(start=ll[i],test=test,predict=predict,days=day,this=this,periods=6)
         dflist.append(df)
     df = pd.concat(dflist)
-    df.to_csv(r'bp_test_result.csv')
-    
-    ###GBDT 测试
-    dflist = []
-    for i in range(len(ll)):
-        gbdt = gbdt_tt(start=ll[i],train=False,test=test,predict=predict,days=day)
-        dflist.append(gbdt)
-    print(len(dflist))
-    gbdt = pd.concat(dflist)
-    gbdt.to_csv(r'gbdt_test_result.csv')
-    
-    ##线性回归
-    df = df.reset_index().set_index(['intersection_id','tollgate_id','time_window_s'])
-    gbdt = gbdt.reset_index().set_index(['intersection_id','tollgate_id','time_window_s'])
-    df = df.join(gbdt[['gbdt']])
-    df.to_csv('bp_gbdt.csv')
-    if reg:
-        feature = ['pred','bp','gbdt']
-        paramlist = linear_reg(fdir = r'bp_gbdt.csv',feature =feature,predict=predict)
-        print(paramlist)
-        print(paramlist['pred'])
-        print(paramlist['bp'])
-        print(paramlist['gbdt'])
-        print(df.dtypes)
-        df['bp']=paramlist['pred']*df.loc[:,'pred'] + paramlist['bp']*df.loc[:,'bp'] +paramlist['gbdt']*df.loc[:,'gbdt']
-        GLOBAL['linear'] = paramlist
-        GLOBAL = cache(GLOBAL)
-    else:
-        paramlist = GLOBAL['linear']
-        print(paramlist)
-        df['bp']=paramlist['pred']*df.loc[:,'pred'] + paramlist['bp']*df.loc[:,'bp'] +paramlist['gbdt']*df.loc[:,'gbdt']
-    df.to_csv(r'after_linear.csv')
+    df.to_csv(''.join([this,r'_test_result.csv']))
     
     modifyed = []
-    df = load_volume(fdir=r'after_linear.csv')
+    df = load_volume(fdir=''.join([this,r'_test_result.csv']))
     df = df[pd.notnull(df['bp'])]
     for i in range(len(ll)):
         df1 = bp_modification(df,start=ll[i],predict=predict,days=day,method=3)   
         modifyed.append(df1)
     df = pd.concat(modifyed)
     df.index.name = 'time_window_s'
-    df.to_csv(r'bp_modifyed.csv')
-#    
+    df.to_csv(''.join([this,r'_test_result.csv']))
 #    
     #evalute
-    df = load_volume(fdir=r'bp_modifyed.csv')
+    df = load_volume(fdir=''.join([this,r'_test_result.csv']))
     df = df[pd.notnull(df['bp'])]
-    for i in range(len(testts)):
-        evalute(df,start=testts[i],predict=predict,days=7)
-#        
-#        
-    bp_final_agg(fdir=r'bp_modifyed.csv')
-#    
-    
-        
+    for i in range(len(ll)):
+        evalute(df,start=ll[i],predict=predict,days=7)
         
     '''
     输入点为当前时间t
